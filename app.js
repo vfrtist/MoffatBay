a0d82b927009aa5b0d1b09774c233c8ccb4e9a0e
vfrtist <vfrtist@aol.com>
2025-04-15 09:30:03 -0400

    Updated counter to prevent fewer than 1 guest

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const down = document.querySelector('#down')
 const guests = document.querySelector('#guests')
 
 
+function checkForOne() {
+    if (guests.value < 1) {
+        guests.value == 1
+        down.disabled = true
+    } else if (guests.value == 1) {
+        down.disabled = true
+    } else {
+        down.disabled = false
+    }
+}
+
 up.addEventListener('click', (e) => {
     guests.value = parseInt(guests.value) + 1
+    checkForOne()
 })
 down.addEventListener('click', (e) => {
     let val = guests.value
-    if (val > 0) {
+    if (val > 1) {
         guests.value = parseInt(guests.value) - 1
     }
-
+    checkForOne()
 })
+
+guests.addEventListener('change', checkForOne)
