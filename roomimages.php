c9022445efa1f02d7a035f4a55e920551096e030
vfrtist <vfrtist@aol.com>
2025-04-15 10:43:01 -0400

    Created image map for room cards
    Updated image names to match sql names and moved Card into component folder.

---
+++ roomimages.php
@@ -0,0 +1,10 @@
+<?php
+$roomImages = [
+    "Deluxe Queen" => "images/Deluxe Queen.jpg",
+    "Deluxe King" => "images/Deluxe King.jpg",
+    "Lagoon Queen" => "images/Lagoon Queen.jpeg",
+    "Lagoon King" => "images/Lagoon King.jpg",
+    "Luxury Queen" => "images/Luxury Queen.jpg",
+    "Luxury King" => "images/Luxury King.jpg",
+]
+    ?>
