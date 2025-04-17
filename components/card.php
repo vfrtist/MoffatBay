18b01d4082107198c586180b6df7ffda9deb838f
vfrtist <vfrtist@aol.com>
2025-04-17 14:58:36 -0400

    finished new card styling

--- components/card.php
+++ components/card.php
@@ -1,31 +1,33 @@
 <div class="card container vertical">
-    <h2><?php echo $room['typeName'] ?></h2>
-    <div class="subcard">
-        <div class="imgWrapper">
-            <img src="<?php
-            require 'config/roomimages.php';
-            echo $roomImages[$room['typeName']];
-            ?>" alt="A really nice hotel bedroom">
-        </div>
-        <span class="desciption">
+    <div class="imgWrapper">
+        <img src="<?php
+        require 'config/roomimages.php';
+        echo $roomImages[$room['typeName']];
+        ?>" alt="A really nice hotel bedroom">
+    </div>
+    <div class="info container vertical">
+        <h2><?php echo $room['typeName'] ?></h2>
+        <div class="desciption">
             <?php echo $room['description'] ?>
-        </span>
-        <ul class="tags container horizontal">
-            <?php
-            foreach (explode(",", $room['amenities']) as $item) {
-                ?>
-                <li><?php echo trim($item) ?></li>
-            <?php } ?>
-        </ul>
-        <div class="container vertical purchase">
-            <h4 class="price">$<?php echo $room['dailyRate'] ?>/night</h4>
-            <form class="roomForm" action="confirm_test.php">
-                <input type="hidden" name="checkin">
-                <input type="hidden" name="checkout">
-                <input type="hidden" name="guests">
-                <input type="hidden" name="typeName" value="<?php echo htmlspecialchars($room['typeName']); ?>">
-                <button type="submit" class="cta1">Book this room</button>
-            </form>
+        </div>
+        <div class="container horizontal">
+            <ul class="tags container horizontal">
+                <?php
+                foreach (explode(",", $room['amenities']) as $item) {
+                    ?>
+                    <li><?php echo trim($item) ?></li>
+                <?php } ?>
+            </ul>
+            <div class="container vertical purchase">
+                <h4 class="price">$<?php echo $room['dailyRate'] ?>/night</h4>
+                <form class="roomForm" action="confirm_test.php">
+                    <input type="hidden" name="checkin">
+                    <input type="hidden" name="checkout">
+                    <input type="hidden" name="guests">
+                    <input type="hidden" name="typeName" value="<?php echo htmlspecialchars($room['typeName']); ?>">
+                    <button type="submit" class="cta1">Book this room</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
