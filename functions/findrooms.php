3ee911c783bee61e9f9f0f28948eb502295f521f
vfrtist <137813892+vfrtist@users.noreply.github.com>
2025-04-19 13:05:32 -0400

    corrected room search
    Now filters by guest size and dates correctly. In and out were backwards before.

--- functions/findrooms.php
+++ functions/findrooms.php
@@ -1,6 +1,6 @@
 <?php
 
-function find_rooms($in, $out)
+function find_rooms($in, $out, $guests)
 {
     require_once 'config/db.php';
     $db = new Database();
@@ -8,21 +8,22 @@ function find_rooms($in, $out)
     SELECT r.roomNumber, rt.typeID, rt.typeName, rt.description, rt.amenities, rt.maxOccupancy, rt.dailyRate
     FROM Room r
     JOIN RoomType rt ON r.roomType = rt.typeID
-    WHERE r.roomNumber = (
+    WHERE rt.maxOccupancy >= ?
+    AND r.roomNumber = (
         SELECT MIN(r2.roomNumber)
         FROM Room r2
         WHERE r2.roomType = r.roomType
             AND r2.roomNumber NOT IN (
                 SELECT res.roomNumber
                 FROM Reservation res
-                WHERE NOT (
-                    res.checkout <= ? OR
-                    res.checkin >= ?
+                WHERE (
+                    res.checkout >= ? AND
+                    res.checkin <= ?
                 )
             )
         )';
-    $types = 'ss';
-    $params = [$in, $out];
+    $types = 'iss';
+    $params = [$guests, $in, $out];
     $result = $db->query($query, $types, $params);
     $_SESSION['available_rooms'] = [];
     foreach ($result as $r) {
--- search.php
+++ search.php
@@ -17,15 +17,19 @@
         <?php
         require 'functions/findrooms.php';
         // if someone got here via searching enter dates.
-        if (isset($_GET['checkin']) && isset($_GET['checkout'])) {
-            find_rooms($_GET['checkin'], $_GET['checkout']);
+        if (isset($_GET['checkin']) && isset($_GET['checkout']) && ($_GET['guests'])) {
+            find_rooms($_GET['checkin'], $_GET['checkout'], $_GET['guests']);
         } else {
             $in = date('Y-m-d', strtotime('+1 day'));
             $out = date('Y-m-d', strtotime('+2 day'));
-            find_rooms($in, $out);
+            find_rooms($in, $out, 2);
         }
-        foreach ($_SESSION['available_rooms'] as $room) {
-            include 'components/card.php';
+        if (count($_SESSION['available_rooms']) == 0) { ?>
+            <div>No available rooms under those terms. Please search again.</div>
+        <?php } else {
+            foreach ($_SESSION['available_rooms'] as $room) {
+                include 'components/card.php';
+            }
         }
         ?>
     </main>
