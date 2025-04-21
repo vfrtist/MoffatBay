4814b47ae73bd7ce1c8d9da46f715d97690a8d7d
vfrtist <137813892+vfrtist@users.noreply.github.com>
2025-04-21 19:46:11 -0400

    Snugged proportions and added feedback to search

--- search.js
+++ search.js
@@ -4,6 +4,8 @@ const guests = document.querySelector('#guests')
 const forms = document.querySelectorAll('.roomForm')
 const up = document.querySelector('#up')
 const down = document.querySelector('#down')
+const searchForm = document.querySelector('#search')
+const searchButton = searchForm.querySelector('button[type="submit"]')
 
 if (forms) {
     forms.forEach((form) => {
@@ -26,16 +28,28 @@ function checkForOne() {
     }
 }
 
+function newSearch() {
+    if (location.pathname.includes('search')) {
+        searchButton.innerHTML = 'Update Search'
+    }
+}
+
 up.addEventListener('click', (e) => {
     guests.value = parseInt(guests.value) + 1
     checkForOne()
+    newSearch()
 })
 down.addEventListener('click', (e) => {
     let val = guests.value
     if (val > 1) {
         guests.value = parseInt(guests.value) - 1
+        newSearch()
     }
     checkForOne()
 })
 
 guests.addEventListener('change', checkForOne)
+
+searchForm.querySelectorAll('input').forEach((input) => {
+    input.addEventListener('change', newSearch)
+})
