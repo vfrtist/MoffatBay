f3a428599a495e45eec9edbf87ad1772d7787bd2
Wau-DSC <superjedi224@gmail.com>
2025-04-25 12:07:11 -0400

    Correct typo & change labels to use html label tags

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<!--Code to Handele Incoming Message Here - Maybe relay to email?-->
+<!--Code to Handle Incoming Message Here - Maybe relay to email?-->
 
 <head>
 	<?php require 'components/head.php' ?>
@@ -12,26 +12,25 @@
 <body>
 	<?php require 'components/fullheader.php' ?>
 	<main>
-		<?php if ($_SERVER["REQUEST_METHOD"] == "POST") { ?>
+		<?php if($_SERVER["REQUEST_METHOD"] == "POST"){ ?>
 			<p>Your message has been received. Thank you for contacting Moffat Bay Lodge.</p>
-		<?php } else { ?>
+		<?php }else{ ?>
 			<img style="float:left;margin-right:3px" height="150px" src="images/k-lee-corey-PSTo0tGYzts-unsplash.jpg" />
 			<div>
 				<h2>Contact Us</h2>
-				Moffat Bay Lodge<br />
-				1 Lodge Road<br />
-				Moffat Bay, ON 00000<br />
-				555-555-5555<br /><br />
+				Moffat Bay Lodge<br/>
+				1 Lodge Road<br/>
+				Moffat Bay, ON 00000<br/>
+				555-555-5555<br/><br/>
 			</div>
 			<form action="contact.php" method="post" style="font-size:0.8em;clear:both;margin-left:6px">
-				Name:
+				<label for="name">Name:</label>
 				<input required type="text" name="name">
-				Email:
+				<label for="email">Email:</label>
 				<input required type="text" name="email">
-				Phone (optional):
+				<label for="phone">Phone (optional):</label>
 				<input type="text" name="phone">
-				<textarea required name="message" style="display:block" cols="100" rows="10"
-					placeholder="Your message here."></textarea>
+				<textarea required name="message" style="display:block" cols="100" rows="10" placeholder="Your message here."></textarea>
 				<input type="submit" value="Contact">
 			</form>
 		<?php } ?>
